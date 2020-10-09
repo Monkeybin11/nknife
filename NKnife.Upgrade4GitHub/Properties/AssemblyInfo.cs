62ab341d0b283e5f9b274153118899a599d7fa5d
Erian-NotePC <lukan@xknife.net>
2020-10-09 16:49:11 +0800

    .

--- NKnife.Upgrade4GitHub/Properties/AssemblyInfo.cs
+++ NKnife.Upgrade4GitHub/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("NKnife.Win.AutoUpdaterFromGitHub")]
+[assembly: AssemblyTitle("NKnife.Upgrade4Github")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NKnife.Win.AutoUpdaterFromGitHub")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCompany("xknife.net")]
+[assembly: AssemblyProduct("NKnife.Upgrade4Github")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.1010")]
-[assembly: AssemblyFileVersion("2.1.0.1010")]
+[assembly: AssemblyVersion("2.1.0.109")]
+[assembly: AssemblyFileVersion("2.1.0.109")]
